b873e9b691b55a130d34b969d424b9c16bdf1e22
Alexandre Augusto Arduini Tavares <arduinitavares@gmail.com>
2026-05-30 09:41:05 -0300

    Refactor GitHub Actions workflow, add Cypress configuration and tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,17 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    testIsolation: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
